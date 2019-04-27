4f789f675cafb3279eec5d88d4cb9f98d2ff3906
chuan <charlie890414@gmail.com>
2019-04-27 14:17:35 +0800

    sample

---
+++ models/recipe.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+var recipe = mongoose.Schema({
+    name     : String,
+    message     : String
+});
+
+/* export module */
+module.exports = mongoose.model( 'recipe', recipe );
--- server.js
+++ server.js
@@ -1,9 +1,34 @@
+const MithVaultSDK = require('./dist/mith-vault-sdk.min.js');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = new express();
+const recipe = require('./models/recipe');
+
+const clientId = '0bb0dc2308c147f3a6b9f47aee5fbf5e';
+const clientSecret = '6bbd412f129674644d12c075336f1a7285cb586770e904049acc184174e25e66e036427201fab9a6dd2e3df81503e5eafd6750573fd5def49e695bf0d4511c88';
+const miningKey = 'cf';
+const sdk = new MithVaultSDK({ clientId, clientSecret, miningKey });
+
+// sdk.getUserMiningAction({ token: userToken }).then(data => {
+  
+// }).catch(error => {
+  
+// });
 
 app.get('/', function (request, response) {
-    res.status(200).send('OK');
+    response.status(200).send(sdk.getBindURI());
 });
+
+app.get('/success', function (request, response) {
+    console.log(req.query.grant_code)
+    console.log(req.query.state)
+});
+
+app.get('/faillure', function (request, response) {
+    console.log(req.query.grant_code)
+    console.log(req.query.state)
+});
+
 
 app.listen(3000);
 console.log("Running at Port 3000");
