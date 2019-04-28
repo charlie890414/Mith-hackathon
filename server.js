e21e8af70113409222d2e7dc75e0cad8c8cd5c4c
chuan <charlie890414@gmail.com>
2019-04-28 11:55:36 +0800

    qwe

--- server.js
+++ server.js
@@ -16,88 +16,7 @@ const app = new express();
 const menu = require('./models/menu');
 const movie = require('./models/movie');
 const account = require('./models/account');
-var CryptoJS = require("crypto-js");
-const axios = require('axios');
-
-function _uuid() {
-    var d = Date.now()
-    if (
-        typeof performance !== 'undefined' &&
-        typeof performance.now === 'function'
-    ) {
-        d += performance.now() //use high-precision timer if available
-    }
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = (d + Math.random() * 16) % 16 | 0
-        d = Math.floor(d / 16)
-        return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16)
-    })
-}
-
-function _sendAPI(
-    endpoint,
-    method = 'GET',
-    options = {
-        headers: null,
-        data: null,
-        params: null
-    }
-) {
-    const payload = options.params || options.data || {}
-    const headers = options.headers || {}
-
-    if ('client_id' in payload) {
-        const signature = _generateSignature(
-            payload,
-            CryptoJS.enc.Hex.parse(this.clientSecert)
-        )
-        headers['X-Vault-Signature'] = signature
-    }
-
-    const url = `${this.api}/${endpoint}`
-    try {
-        return axios({
-                method,
-                url,
-                params: options.params,
-                data: options.data,
-                headers
-            })
-            .then(response => response.data)
-            .catch(error => {
-                throw error
-            })
-    } catch (e) {
-        throw e
-    }
-}
-
-function _generateSignature(data, secret) {
-    let query = ''
-    if (data && Array.isArray(data)) {
-        query = data.join(',')
-    } else if (data) {
-        const sortedData = _sortObject(data)
-        query = Object.keys(sortedData)
-            .map(key => `${key}=${sortedData[key]}`)
-            .join('&')
-    } else if (data) {
-        query = data.toString()
-    }
-    const signature = CryptoJS.HmacSHA512(query, secret)
-    return CryptoJS.enc.Hex.stringify(signature)
-}
-
-function _sortObject(o) {
-    return Object.keys(o)
-        .sort()
-        .reduce((r, k) => ((r[k] = o[k]), r), {})
-}
-
-function _randomInt() {
-    return Math.floor(Math.random() * 1e17)
-}
-
+const crypto = require('crypto');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -107,44 +26,6 @@ var storage = multer.diskStorage({
         cb(null, Date.now() + "-" + file.originalname);
     }
 });
-
-function _sendAPI(
-    endpoint,
-    method = 'GET',
-    options = {
-        headers: null,
-        data: null,
-        params: null
-    }
-) {
-    const payload = options.params || options.data || {}
-    const headers = options.headers || {}
-
-    if ('client_id' in payload) {
-        const signature = _generateSignature(
-            payload,
-            CryptoJS.enc.Hex.parse(this.clientSecert)
-        )
-        headers['X-Vault-Signature'] = signature
-    }
-
-    const url = `${this.api}/${endpoint}`
-    try {
-        return axios({
-                method,
-                url,
-                params: options.params,
-                data: options.data,
-                headers
-            })
-            .then(response => response.data)
-            .catch(error => {
-                throw error
-            })
-    } catch (e) {
-        throw e
-    }
-}
 
 var upload = multer({
     storage: storage
@@ -369,7 +250,7 @@ app.post('/mining', function (request, response) {
             console.log(error);
             response.status(400).send("Error" + error);
         });
-    });
+    });    
 });
 
 app.post('/mining', function (request, response) {
@@ -387,30 +268,25 @@ app.post('/mining', function (request, response) {
             console.log(error);
             response.status(400).send("Error" + error);
         });
-    });
+    });    
 });
 
 app.get('/userinfo', function (request, response) {
     account.findOne({
         '_id': request.query.user_id
     }).exec(function (err, result) {
-        const payload = {
-            client_id: clientId,
-            timestamp: Math.floor(Date.now() / 1000),
-            nonce: _randomInt()
-        }
-
-        const headers = {
-            Authorization: result.token
-        }
-        const res = _sendAPI.call(this, 'oauth/user-info', 'GET', {
-            headers,
-            params: payload
-        })
-
-        response.send(res);
-    });
+        sdk.getUserInformation({
+            token: result.token
+        }).then(data => {
+            console.log(data);
+            response.status(200).send(data.balance);
+        }).catch(error => {
+            console.log(error);
+            response.status(400).send("Error" + error);
+        });
+    });    
 });
 
 app.listen(5000);
 console.log("Running at Port 5000");
+
